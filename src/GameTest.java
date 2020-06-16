ba1a373441dcaf3488e33bf31ccc0448d67bf1e2
mateus <mateuspaivamatiazzi1@gmail.com>
2020-06-16 13:08:41 -0400

    Mudança total

---
+++ src/GameTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+
+    private Game game;
+
+    @Before
+    public void setUp(){
+        game = new Game();
+    }
+
+    @Test
+    public void canScoreGutterGame(){
+        game.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertEquals(game.score(), 0);
+    }
+
+    @Test
+    public void canScoreGameOfOnes(){
+        game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
+        assertEquals(game.score(), 20);
+    }
+
+    @Test
+    public void canScoreSpareFollowedByThree(){
+        game.roll(5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertEquals(game.score(), 16);
+    }
+
+    @Test
+    public void canScoreStrikeFollowedByThreeThenThree(){
+        game.roll(10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertEquals(game.score(), 22);
+    }
+
+    @Test
+    public void canScorePerfectGame(){
+        game.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+        assertEquals(game.score(), 300);
+    }
+}
--- src/SistemaDePontuacao.java
+++
@@ -1,6 +0,0 @@
-public class SistemaDePontuacao {
-
-    public int retornarPontuacaoDoJogadorComMenosDeDezPinos(Frame frame){
-        return frame.getPontuacaoDaPrimeiraBola() + frame.getPontuacaoDaSegundaBola();
-    }
-}
